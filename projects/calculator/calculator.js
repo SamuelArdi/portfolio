5bbe313a50a13b3ad3c7e67bf488cb2a19c12677
SamuelArdi <112629940+canotice@users.noreply.github.com>
2023-12-09 22:31:52 +0800

    bug fixes & quality of life changes

--- projects/calculator/calculator.js
+++ projects/calculator/calculator.js
@@ -1,13 +1,22 @@
 let equalPressed = 0;
+let clearPressed = 0;
+let error = false;
+
 let buttonInput = document.querySelectorAll("button");
+let display = document.getElementsByClassName("display")
+
 let input = document.getElementById("input");
 let equal = document.getElementById("equal");
 let clear = document.getElementById("clear");
 let erase = document.getElementById("delete");
 
+let currentInputSize = 72;
+currentInputSize = Number(currentInputSize);
+
 window.onload = function () {
 
     input.value = "";
+    input.style.fontSize = `${currentInputSize}px`;
 
 };
 
@@ -16,17 +25,60 @@ buttonInput.forEach(function (button) {
     button.addEventListener("click", function() {
 
         input.placeholder = "0";
-        
-        if (equalPressed == 0) {
+        error = false;
 
-            input.value += button.value;
+        if (error == false) {
+        
+            input.style.fontSize = `${currentInputSize}px`;
 
         }
-        else if (equalPressed == 1) {
 
-            //input.value = "";
+        input.value += button.value;
+        
+        if (input.scrollWidth > input.clientWidth) {
+            
+            currentInputSize -= 6;
+            
+            input.style.fontSize = `${currentInputSize}px`;
+            
+            console.log("decreased: " + currentInputSize);
+            
+        }
+        
+        if (currentInputSize <= 36) {
+
+            let currentValueOnDisplay = input.value;
+
+            input.value = "";
+            input.value = currentValueOnDisplay;
+
+            currentInputSize = 36;
+
+            input.style.fontSize = "36px";
+
+        }
+
+        if (clearPressed == 1) {
+
+            clearPressed = 0;
+            currentInputSize = 72;
+
+            input.style.fontSize = "72px";
+
+        }
+
+        // Start From here
+        // date: 12/9/2023
+        if (currentInputSize === 36 && equalPressed == 0) {
+            
+            input.value = input.value.substr(0, input.value.length - 1);
+            
+        }
+        
+        if (equalPressed == 1) {
+        
             equalPressed = 0;
-
+        
         }
 
     })
@@ -36,47 +88,84 @@ buttonInput.forEach(function (button) {
 equal.addEventListener("click", function() {
     
     equalPressed = 1;
-    let inputValue = input.value;
     
     try {
-        
-        let solution = eval(inputValue);
-        
-        if (Number.isNaN(solution) || !Number.isFinite(solution)) {
+
+        let result = eval(input.value);
+
+        if (!Number.isFinite(result)) {
             
+            input.value = "";
+            input.placeholder = "Invalid Expression";
+
+        }
+        
+        if (typeof result === "number") {
+        
+            input.value = result;
+
+        }
+        else {
+
             input.value = "";
             input.placeholder = "Syntax Error";
 
         }
 
-        if (Number.isInteger(solution)) {
-            
-            input.value = solution;
-
-        } 
-
-        else {
-
-            input.value = solution.toFixed(2);
-
-        }
-
-    }
-    catch (error) {
-
-        input.placeholder = "No Value";
+        // Secret message
         
     }
+    catch (error) {
+        
+        error = true;
+
+        if (input.value === "77.77(77.77)") {
+
+            input.style.fontSize = "24.3px";
+            
+            input.value = "";
+            input.placeholder = "How the fuck did you manage to find this";
+    
+        }
+        else{
+
+            input.value = "";
+            input.placeholder = "Syntax Error";
+
+        }
+        
+    }
+
 
 })
 
 clear.addEventListener("click", function() {
 
+    clearPressed = 1;
+
+    input.style.fontSize = "72px";
     input.value = "";
 
 })
 
 erase.addEventListener("click", function() {
+
+    if (input.scrollWidth == input.clientWidth) {
+    
+        currentInputSize += 6;
+
+        input.style.fontSize = `${currentInputSize}px`;
+
+        console.log("increased: " + currentInputSize)
+
+    }
+
+    if (currentInputSize > 72) {
+
+        currentInputSize = 72;
+        input.style.fontSize = `${currentInputSize}px`;
+
+    }
 
     input.value = input.value.substr(0, input.value.length - 1);
 
