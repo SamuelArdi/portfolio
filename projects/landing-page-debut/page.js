a95882b83e3626c06375b424102371c472c75088
SamuelArdi <149386215+SamuelArdi@users.noreply.github.com>
2024-05-10 11:19:40 +0800

    Hamburger menu is now working

--- projects/landing-page-debut/page.js
+++ projects/landing-page-debut/page.js
@@ -14,3 +14,35 @@ window.addEventListener("load", () => {
     }, 1700);
 
 });
+
+var hamburgerMenuIcon = document.querySelector(".hamburgerMenu");
+var backIcon = document.querySelector(".hbs-back");
+
+var menuOverlay = document.querySelector(".hamburgerMenu-overlay");
+var menuShelf = document.querySelector(".hamburgerMenu-shelf");
+
+var shelfState = menuShelf.dataset.state;
+
+hamburgerMenuIcon.addEventListener("click", () => {
+
+    menuOverlay.style.transform = "translateX(0)";
+
+    setTimeout(() => {
+        menuShelf.style.transform = "translateX(0)";
+    }, 500);
+
+    shelfState = "opened"
+
+})
+
+backIcon.addEventListener("click", () => {
+
+    menuShelf.style.transform = "translateX(100%)";
+
+    setTimeout(() => {
+        menuOverlay.style.transform = "translateX(100%)";
+    }, 800);
+
+    shelfState = "closed"
+
+})
